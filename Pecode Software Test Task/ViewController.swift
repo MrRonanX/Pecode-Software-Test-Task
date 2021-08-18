4952eeec5e8eb96ebf4fc7d35624792a0fb0b3b1
Roman Kavinskyi <mrronanx@gmail.com>
2021-08-18 12:08:28 +0700

    Initial Commit

---
+++ Pecode Software Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pecode Software Test Task
+//
+//  Created by Roman Kavinskyi on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
